e4db673c4994b8406e9515ae507763d0150b452b
longzhis <yux166@ucsd.edu>
2019-02-27 13:52:19 -0800

    change class name

--- app/src/main/java/edu/ucsd/cse110/personalbest/IntentionalWalk.java
+++ app/src/main/java/edu/ucsd/cse110/personalbest/IntentionalWalk.java
@@ -1,22 +1,22 @@
 package edu.ucsd.cse110.personalbest;
 
-public class IntentionalStep implements Step {
+public class IntentionalWalk implements Step {
     private int step;
     private long timeCurrent; //Change to relevant time class if necessary
     private long timeStart; //Change to relevant time class if necessary
     private long timeElapsed; //Change to relevant time class if necessary
     //Dummy default constructor; do not use
-    IntentionalStep(){
+    IntentionalWalk(){
         this.step=0;
     }
     //Constructor for user pressing start
-    IntentionalStep(long timeStart){
+    IntentionalWalk(long timeStart){
         this.step=0;
         this.timeStart=timeStart;
     }
     //Constructor for intentional step collection for the bar chart statistics; create one when one
     //intentional walk is finished and update this when others are finished
-    IntentionalStep(int step,long timeElapsed){
+    IntentionalWalk(int step, long timeElapsed){
         this.step=step;
         this.timeStart=0; //Don't care
         this.timeCurrent=0; //Don't care
