ab76872d74d10acf27112ed5d7ad20fb2a7216f8
richardmatthias <richardmatthias@126.com>
2019-03-05 15:11:38 -0800

    Added helpers for int/step conversion. Added manager for SharedPreferences. Fixed various naming and refactoring issues

---
+++ app/src/main/java/edu/ucsd/cse110/personalbest/Managers/SharedPrefManager.java
@@ -0,0 +1,63 @@
+package edu.ucsd.cse110.personalbest.Managers;
+
+import android.content.SharedPreferences;
+
+import edu.ucsd.cse110.personalbest.Exercise;
+import edu.ucsd.cse110.personalbest.Goal;
+import edu.ucsd.cse110.personalbest.Step;
+import edu.ucsd.cse110.personalbest.User;
+import edu.ucsd.cse110.personalbest.Walk;
+
+import static edu.ucsd.cse110.personalbest.Helpers.StepIntHelper.*;
+
+public class SharedPrefManager {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    public static final String USER_GOAL_KEY = "userGoal";
+    public static final String USER_GOAL_HISTORY_KEY = "userGoalHistory";
+    public static final String USER_HEIGHT_KEY = "userHeight";
+    public static final String USER_WALK_KEY = "userWalk";
+    public static final String USER_WALK_HISTORY_KEY = "userWalkHistory";
+    public static final String USER_EXERCISE_KEY = "userExercise";
+    public static final String USER_EXERCISE_HISTORY_KEY = "userExerciseHistory";
+
+    SharedPrefManager(SharedPreferences sharedPreferences){
+        this.sharedPreferences=sharedPreferences;
+        this.editor=sharedPreferences.edit();
+    }
+
+    public void publishData(User user){
+        editor.putInt(USER_GOAL_KEY,user.getGoal().getStep());
+        editor.putInt(USER_EXERCISE_KEY,user.getCurrentExercise().getStep());
+        editor.putInt(USER_WALK_KEY,user.getCurrentWalk().getStep());
+        editor.putInt(USER_HEIGHT_KEY,user.getHeight());
+        storeIntArray(USER_GOAL_HISTORY_KEY,extractStepArray(user.getGoalHistory()));
+        storeIntArray(USER_EXERCISE_HISTORY_KEY,extractStepArray(user.getExerciseHistory()));
+        storeIntArray(USER_WALK_HISTORY_KEY,extractStepArray(user.getWalkHistory()));
+        editor.commit();
+    }
+
+    public void retrieveData(User user){
+        user.setGoal(new Goal(sharedPreferences.getInt(USER_GOAL_KEY,5000)));
+        user.setGoalHistory(constructGoalArray(parseIntArray(USER_GOAL_HISTORY_KEY)));
+        user.setCurrentWalk(new Walk(sharedPreferences.getInt(USER_WALK_KEY,0)));
+        user.setWalkHistory(constructWalkArray(parseIntArray(USER_WALK_HISTORY_KEY)));
+        user.setCurrentExercise(new Exercise(sharedPreferences.getInt(USER_EXERCISE_KEY,0)));
+        user.setExerciseHistory(constructExerciseArray(parseIntArray(USER_EXERCISE_HISTORY_KEY)));
+        user.setHeight(sharedPreferences.getInt(USER_HEIGHT_KEY,180));
+    }
+
+    private void storeIntArray(String baseKey,int[] intArray){
+        for(int i=0;i<intArray.length;i++){
+            editor.putInt(baseKey+i,intArray[i]);
+        }
+    }
+
+    private int[] parseIntArray(String baseKey){
+        int[] retArray=new int[7];
+        for(int i=0;i<retArray.length;i++){
+            retArray[i]=sharedPreferences.getInt(baseKey+i,0);
+        }
+        return retArray;
+    }
+}
--- app/src/main/java/edu/ucsd/cse110/personalbest/User.java
+++ app/src/main/java/edu/ucsd/cse110/personalbest/User.java
@@ -6,18 +6,70 @@ public class User {
     public static int trackLength=7; //The number of days we track a user
     public static final int startingGoal=5000; //The starting goal of a new user
     private Goal goal;
-    private IncidentalWalk stepsToday;
-    private IntentionalWalk currentWalk;
-    private IncidentalWalk[] stepsHistory;
-    private IntentionalWalk[] walkHistory;
+    private Walk currentWalk;
+    private Exercise currentExercise;
+    private Walk[] walkHistory;
+    private Exercise[] exerciseHistory;
     private Goal[] goalHistory;
+    private int height;
 
     // User constrcutor
     User(){
-        this.goal=new Goal(startingGoal);
-        this.stepsToday=new IncidentalWalk();
-        this.stepsHistory=new IncidentalWalk[trackLength];
-        this.walkHistory=new IntentionalWalk[trackLength];
-        this.goalHistory=new Goal[trackLength];
+    }
+
+    public Goal getGoal(){
+        return goal;
+    }
+
+    public Goal[] getGoalHistory(){
+        return goalHistory;
+    }
+
+    public Walk getCurrentWalk(){
+        return currentWalk;
+    }
+
+    public Walk[] getWalkHistory(){
+        return walkHistory;
+    }
+
+    public Exercise getCurrentExercise(){
+        return currentExercise;
+    }
+
+    public Exercise[] getExerciseHistory(){
+        return exerciseHistory;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void setHeight(int height){
+        this.height=height;
+    }
+
+    public void setGoal(Goal goal){
+        this.goal=goal;
+    }
+
+    public void setGoalHistory(Goal[] goalHistory){
+        this.goalHistory=goalHistory;
+    }
+
+    public void setCurrentWalk(Walk currentWalk){
+        this.currentWalk=currentWalk;
+    }
+
+    public void setCurrentExercise(Exercise currentExercise){
+        this.currentExercise=currentExercise;
+    }
+
+    public void setWalkHistory(Walk[] walkHistory){
+        this.walkHistory=walkHistory;
+    }
+
+    public void setExerciseHistory(Exercise[] exerciseHistory){
+        this.exerciseHistory=exerciseHistory;
     }
 }
