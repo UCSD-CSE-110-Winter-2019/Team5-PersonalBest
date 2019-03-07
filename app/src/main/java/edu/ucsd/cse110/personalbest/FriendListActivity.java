950fe2a13f883d0733413e0e39be664a61900b8f
Chingyen Huang <chh240@ucsd.edu>
2019-03-06 21:54:06 -0800

    Issue #90
    update fire store

--- app/src/main/java/edu/ucsd/cse110/personalbest/FriendListActivity.java
+++ app/src/main/java/edu/ucsd/cse110/personalbest/FriendListActivity.java
@@ -6,13 +6,24 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FriendListActivity extends AppCompatActivity {
 
-    String COLLECTION_KEY = "users";
+    String COLLECTION_KEY = "kk";
+    String REQ_KEY = "Requested friends";
+    String LIST_KEY = "Friend list";
+    String REQ_EMAIL_KEY = "Requested Email";
+    String FRIEND_EMAIL_KEY = "Friend's Email";
 
     CollectionReference users;
+    DocumentReference doc;
+    CollectionReference request;
+    CollectionReference friendlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,16 @@ public class FriendListActivity extends AppCompatActivity {
         });
 
         users = FirebaseFirestore.getInstance().collection(COLLECTION_KEY);
-        users.document(MainActivity.user.getEmailAddress());
+        doc = users.document(MainActivity.user.getEmailAddress());
+        request = doc.collection(REQ_KEY);
+        friendlist = doc.collection(LIST_KEY);
+
+        Map<String, String> reqEmail = new HashMap<>();
+        reqEmail.put(REQ_EMAIL_KEY, "ziw330@ucsd.edu");
+        request.add(reqEmail);
+
+        Map<String, String> friEmail = new HashMap<>();
+        friEmail.put(FRIEND_EMAIL_KEY, "xig113@ucsd.edu");
+        friendlist.add(friEmail);
     }
 }
